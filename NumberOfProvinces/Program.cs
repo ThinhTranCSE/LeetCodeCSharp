8b84d1c02149683d7ccc6d59267d32e9b928a35a
ThinhTranCSE <152634119+ThinhTranCSE@users.noreply.github.com>
2026-02-03 22:50:28 +0700

    NumberOfProvinces

---
+++ NumberOfProvinces/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace NumberOfProvinces;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[][] isConnected = new int[][]
+        {
+            new int[] { 1, 1, 0 },
+            new int[] { 1, 1, 0 },
+            new int[] { 0, 0, 1 }
+        };
+        int result = solution.FindCircleNum(isConnected);
+        Console.WriteLine(result);
+    }
+}
+
+public class Solution
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
+        var visited = new HashSet<int>();
+        int result = 0;
+        for (int city = 0; city < isConnected.Length; city++)
+        {
+            if (visited.Contains(city))
+            {
+                continue;
+            }
+            var stack = new Stack<int>();
+            stack.Push(city);
+            while (stack.Count > 0)
+            {
+                int visitingCity = stack.Pop();
+                if (visited.Contains(visitingCity))
+                {
+                    continue;
+                }
+                visited.Add(visitingCity);
+                var isConnectedCities = isConnected[visitingCity];
+                for (int isConnectedCity = 0; isConnectedCity < isConnectedCities.Length; isConnectedCity++)
+                {
+                    if (isConnectedCities[isConnectedCity] == 1)
+                    {
+                        stack.Push(isConnectedCity);
+                    }
+                }
+            }
+            result++;
+        }
+        return result;
+    }
+}
